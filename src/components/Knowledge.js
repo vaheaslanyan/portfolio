ecace9db20bfeebb08890a49dfa15ec937973665
Vahe Aslanyan <vahe.sde@gmail.com>
2020-03-11 20:29:08 -0700

    add to Knowledge.js

--- src/components/Knowledge.js
+++ src/components/Knowledge.js
@@ -13,62 +13,100 @@ const Knowledge = () => {
       <div className="row row-even">
         <div className="inside-row-div">
           <Container>
-            <Row>
-              <Col>
-                <h3>Languages</h3>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h4>HTML</h4>
-              </Col>
-              <Col>
-                <h4>CSS</h4>
-              </Col>
-              <Col>
-                <h4>JavaScript</h4>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h3>Libraries</h3>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <div className="knowledge-item">
-                  <div className="knowledge-logo-container">
-                    <span className="react-logo">
-                      <span className="nucleo"></span>
-                    </span>
-                    <div></div>
+            <div className="knowledge-content">
+              <Row>
+                <Col>
+                  <h3>Must-haves</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <img src="https://res.cloudinary.com/hbmteb7ks/image/upload/v1583979993/portfolio/html_jqmdao.png"></img>
+                    </div>
+                    <p className="knowledge-item-title">HTML</p>
                   </div>
-                  <p className="knowledge-item-title">React</p>
-                </div>
-              </Col>
-              <Col>
-                <div className="knowledge-item">
-                  <div className="knowledge-logo-container">
-                    <span className="react-logo">
-                      <span className="nucleo"></span>
-                    </span>
-                    <div></div>
+                </Col>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <img src="https://res.cloudinary.com/hbmteb7ks/image/upload/v1583979993/portfolio/css_bzott5.png"></img>
+                    </div>
+                    <p className="knowledge-item-title">CSS</p>
                   </div>
-                  <p className="knowledge-item-title">React</p>
-                </div>
-              </Col>
-              <Col>
-                <div className="knowledge-item">
-                  <div className="knowledge-logo-container">
-                    <span className="react-logo">
-                      <span className="nucleo"></span>
-                    </span>
-                    <div></div>
+                </Col>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <img src="https://res.cloudinary.com/hbmteb7ks/image/upload/v1583979993/portfolio/js_wfxwvm.png"></img>
+                    </div>
+                    <p className="knowledge-item-title">JavaScript</p>
                   </div>
-                  <p className="knowledge-item-title">React</p>
-                </div>
-              </Col>
-            </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h3>Front-End</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <span className="react-logo">
+                        <span className="nucleo"></span>
+                      </span>
+                    </div>
+                    <p className="knowledge-item-title">React</p>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <p className="ejs-logo-text">EJS</p>
+                    </div>
+                    <p className="knowledge-item-title">Embeded JavaScript</p>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h3>Back-End</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <img src="https://res.cloudinary.com/hbmteb7ks/image/upload/v1583982631/portfolio/nodejs-logo-png-node-js-on-dark-background-1843_sg1d26.png"></img>
+                    </div>
+                    <p className="knowledge-item-title">node.js</p>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <p className="express-logo-text">express</p>
+                    </div>
+                    <p className="knowledge-item-title">express.js</p>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="knowledge-item">
+                    <div className="knowledge-logo-container">
+                      <img src="https://res.cloudinary.com/hbmteb7ks/image/upload/v1583983303/portfolio/mongodb-logo-white_anvha0.png"></img>
+                    </div>
+                    <p className="knowledge-item-title">mongoDB</p>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h3>Libraries</h3>
+                </Col>
+              </Row>
+            </div>
           </Container>
         </div>
       </div>
