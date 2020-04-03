ac4c14acf9d59f5c02225a927d691a1c8953ad11
Vahe Aslanyan <vahe.sde@gmail.com>
2020-04-02 20:50:04 -0700

    add comment description next to bootstrap import in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //importing bootstrap here in index.js before any app css sheets ensures styles matching between production and development. That's because, all your vendor css (node_modules) stuff is bundled in a separate css file then your application styles in the production build (npm run build). So if we don't put the vendor css before all of our styles, we may get different UI in development and in production. Because, in the development environment, CRA preserves the order of styles while loading (in this case, application styles first and vendor styles later) but in production, the vendor styles loaded first and application styles later.
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
